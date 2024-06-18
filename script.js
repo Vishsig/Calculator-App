3082883f5e5566486d24ec9c4ab27d6e4422530e
Vishwajeet Singh <106250744+Vishsig@users.noreply.github.com>
2024-06-18 20:43:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const input = document.getElementById('input');
+const output = document.getElementById('result');
+let answer = '';
+
+function calculator(el){
+if(el.innerText == '='){
+    output.value = eval(answer);
+    return;
+} 
+if (el.innerText == "Clear"){
+    input.value = '';
+    output.value = '';  
+    return;
+}
+if(el.innerText != "=" && el.innerText != "Clear"){
+    if(el.innerText == "X"){
+        answer += "*";
+    }
+    answer += el.innerText;
+    input.value = answer;
+}
+}
+
