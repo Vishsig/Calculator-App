46103e7816552f936eef2fee19234d3f1d4774b5
Vishwajeet Singh <106250744+Vishsig@users.noreply.github.com>
2024-06-19 14:41:47 +0530

    bug-fix: prev equation re-appear
    previous equations reappeared even after clicking clear button

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ if(el.innerText == '='){
 if (el.innerText == "Clear"){
     input.value = '';
     output.value = '';  
+    answer = '';
     return;
 }
 if(el.innerText != "=" && el.innerText != "Clear"){
